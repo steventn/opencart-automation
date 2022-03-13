5c70b90348b8f9d89982e801356cdde49cf0da9d
steventn <53480750+steventn@users.noreply.github.com>
2022-03-12 19:44:30 -0800

    added user and pass fail tests

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -18,6 +18,28 @@ class TestLogin(Base):
             raise
             print("Error is wrong", format(e))
 
+    def test_user_login_fail(self):
+        driver = self.driver
+        login = LoginPage(driver)
+        login.login(LoginPageData.EMAIL_FAIL, LoginPageData.PASSWORD_PASS)
+
+        try:
+            assert login.get_error_msg() == "Warning: No match for E-Mail Address and/or Password."
+        except Exception as e:
+            raise
+            print("Error is wrong", format(e))
+
+    def test_pass_login_fail(self):
+        driver = self.driver
+        login = LoginPage(driver)
+        login.login(LoginPageData.EMAIL_PASS, LoginPageData.PASSWORD_FAIL)
+
+        try:
+            assert login.get_error_msg() == "Warning: No match for E-Mail Address and/or Password."
+        except Exception as e:
+            raise
+            print("Error is wrong", format(e))
+
     def test_login_pass(self):
         driver = self.driver
         login = LoginPage(driver)
