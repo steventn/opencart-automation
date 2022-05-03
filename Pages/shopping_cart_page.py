6ed999c9c9b1614dcb588e043128bb03fd7bf689
steventn <53480750+steventn@users.noreply.github.com>
2022-05-02 20:06:34 -0700

    begin skeleton for shopping cart flows

---
+++ Pages/shopping_cart_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.support.wait import WebDriverWait
+
+from Resources.locators import RegisterAccountLocators
+from Base.base import Base
+
+class ShoppingCartPage(Base):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    
---
+++ Tests/test_checkout_flow.py
@@ -0,0 +1,9 @@
+from Pages.login_page import LoginPage
+from Base.base import Base
+from Resources.test_data import LoginPageData
+from Resources.locators import MyAccountLocators
+import pytest
+
+
+class TestCheckout(Base):
+
